1776253b38da84a7da8d2ff22b2132b6ed29d241
Angelo <anweisen@web.de>
2023-10-18 23:14:59 +0200

    Implement minecraft var string

--- src/var_int.rs
+++ src/var_int.rs
@@ -1,4 +1,5 @@
-use std::str;
+use std::{io, str};
+use std::io::Write;
 
 use bytes::Buf;
 use tokio::io::AsyncReadExt;
@@ -38,19 +39,41 @@ impl VarInt {
     for i in 0..5 {
       let byte = buffer.get_u8();
 
-      // bitwise and: 01111111 -> deletes the first bit, keeps the rest
+      // bitwise and: 0x7F = 0111 1111 -> deletes the first bit, keeps the rest
       // first bit will be used in order to determine whether to continue
-      decoded_value |= (byte as i32 & 0b01111111) << (i * 7);
+      decoded_value |= (byte as i32 & 0x7F) << (i * 7);
 
-      if byte & 0b10000000 == 0 {
+      // first bit mask: 0x80 = 1000 0000
+      if byte & 0x80 == 0 {
         return Ok(decoded_value);
       }
     }
     Err(VarIntDecodeError::TooLarge)
   }
+
+  pub fn encode(mut value: i32, mut writer: &mut impl Write) -> Result<(), io::Error> {
+    loop {
+      // write 7 bits at a time, msf is continuation bit
+      let mut byte = (value & 0x7F) as u8;
+      value >>= 7;
+
+      // if there are missing bits, add continuation bit
+      if value != 0 {
+        byte |= 0x80;
+      }
+
+      writer.write_all(&[byte])?;
+
+      // there are not further bits, exit
+      if value == 0 {
+        break;
+      }
+    }
+    Ok(())
+  }
 }
 
-pub struct VarString;
+pub struct VarString(pub str);
 
 impl VarString {
   pub fn decode(mut buffer: &mut &[u8]) -> String {
@@ -58,6 +81,12 @@ impl VarString {
     let bytes = &buffer[..length as usize];
     buffer.advance(length as usize);
     str::from_utf8(bytes).unwrap().to_string()
+  }
+
+  pub fn encode(value: String, mut writer: &mut impl Write) -> Result<(), io::Error> {
+    let bytes = value.as_bytes();
+    VarInt::encode(bytes.len() as i32, &mut writer)?;
+    writer.write_all(bytes)
   }
 }
 
